80931cc4daa8d0deb3a00851738748f97c929be3
Laura <lmcsay@gmail.com>
2022-09-24 15:23:57 -0500

    Destructure user from useGetUsersQuery

--- frontend/src/features/users/EditUser.js
+++ frontend/src/features/users/EditUser.js
@@ -7,7 +7,7 @@ import PulseLoader from 'react-spinners/PulseLoader';
 const EditUser = () => {
   const { id } = useParams();
 
-  const user = useGetUsersQuery('usersList', {
+  const { user } = useGetUsersQuery('usersList', {
     selectFromResult: ({ data }) => ({
       user: data?.entities[id],
     }),
