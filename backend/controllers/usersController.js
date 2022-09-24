683c2a299a11c443ea05cc4dea6b8177ead90cfb
Laura <lmcsay@gmail.com>
2022-09-24 13:46:33 -0500

    Make a check for duplicate usernames and note titles that are case insensitive

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -24,7 +24,10 @@ const createNewUser = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: 'All fields are required' });
   }
 
-  const duplicate = await User.findOne({ username }).lean().exec();
+  const duplicate = await User.findOne({ username })
+    .collation({ locale: 'en', strength: 2 })
+    .lean()
+    .exec();
   if (duplicate) {
     return res.status(409).json({ message: 'Duplicate username' });
   }
@@ -61,7 +64,10 @@ const updateUser = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: 'User not found' });
   }
 
-  const duplicate = await User.findOne({ username }).lean().exec();
+  const duplicate = await User.findOne({ username })
+    .collation({ locale: 'en', strength: 2 })
+    .lean()
+    .exec();
   // Allow updates to the original user
   if (duplicate && duplicate?._id.toString() !== id) {
     return res.status(409).json({ message: 'Duplicate username' });
